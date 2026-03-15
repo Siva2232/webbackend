2cdba8a59bce513e3e6d65da198918c2aa651a91
Siva2232 <sivadathanmb@gmail.com>
2026-03-15 16:15:35 +0530

    done

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options("(.*)", cors(corsOptions));
 app.use("/api", limiter);
 
 // IMPORTANT: Handle preflight requests
