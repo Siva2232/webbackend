9a0cb252f43b67b83debd40a56eb0fd3caa6401c
Siva2232 <sivadathanmb@gmail.com>
2026-03-15 15:16:59 +0530

    done

--- utils/qrGenerator.js
+++ utils/qrGenerator.js
@@ -6,9 +6,13 @@ const generateQRCode = async (serialNumber) => {
     const qrData = `${baseUrl}/register-warranty?serial=${serialNumber}`;
     
     const qrImage = await QRCode.toDataURL(qrData, {
-      width: 512,
-      margin: 1,
+      width: 2048,
+      margin: 0,
       errorCorrectionLevel: 'H',
+      color: {
+        dark: '#000000',
+        light: '#ffffff',
+      },
     });
     return qrImage;
   } catch (error) {
