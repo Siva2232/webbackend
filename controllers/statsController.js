f7155e74d3abc45a65d6f0b4a7b2d3fdd022866a
SivaNUAM <sivadathan.mb@nuamenterprises.com>
2026-03-16 23:20:10 +0530

    done

--- controllers/statsController.js
+++ controllers/statsController.js
@@ -1,21 +1,36 @@
 const Product = require("../models/Product");
 const Registration = require("../models/Registration");
 
+const buildRegistrationFilter = (query = {}) => {
+  const { startDate, endDate } = query;
+  const registrationFilter = {};
+
+  if (startDate || endDate) {
+    registrationFilter.registrationDate = {};
+
+    if (startDate) {
+      registrationFilter.registrationDate.$gte = new Date(startDate);
+    }
+
+    if (endDate) {
+      const end = new Date(endDate);
+      end.setHours(23, 59, 59, 999);
+      registrationFilter.registrationDate.$lte = end;
+    }
+  }
+
+  return registrationFilter;
+};
+
+const getRegistrationQuery = (filter = {}) => {
+  return Registration.find(filter)
+    .populate("productId", "productName modelNumber")
+    .sort({ registrationDate: -1 });
+};
+
 exports.getStats = async (req, res) => {
   try {
-    const { startDate, endDate } = req.query;
-    
-    // Filter for registrations
-    let registrationFilter = {};
-    if (startDate || endDate) {
-      registrationFilter.registrationDate = {};
-      if (startDate) registrationFilter.registrationDate.$gte = new Date(startDate);
-      if (endDate) {
-        const end = new Date(endDate);
-        end.setHours(23, 59, 59, 999);
-        registrationFilter.registrationDate.$lte = end;
-      }
-    }
+    const registrationFilter = buildRegistrationFilter(req.query);
 
     const totalProducts = await Product.countDocuments();
     const registeredWarranties = await Registration.countDocuments();
@@ -25,9 +40,7 @@ exports.getStats = async (req, res) => {
     const activeWarranties = registeredWarranties; 
 
     // Fetch the filtered recent registrations
-    const recentRegistrations = await Registration.find(registrationFilter)
-      .populate("productId", "productName")
-      .sort({ registrationDate: -1 })
+    const recentRegistrations = await getRegistrationQuery(registrationFilter)
       .limit(10); // Increased limit for filtered views
 
     res.json({
@@ -35,6 +48,20 @@ exports.getStats = async (req, res) => {
       registeredWarranties,
       activeWarranties,
       recentRegistrations: recentRegistrations || []
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.getRegistrationLedger = async (req, res) => {
+  try {
+    const registrationFilter = buildRegistrationFilter(req.query);
+    const registrations = await getRegistrationQuery(registrationFilter);
+
+    res.json({
+      registrations,
+      total: registrations.length,
     });
   } catch (error) {
     res.status(500).json({ message: error.message });
