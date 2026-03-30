a89102b4dad12f5ae6f33c83db83eb6130a5478a
Siva2232 <sivadathanmb@gmail.com>
2026-03-30 19:42:24 +0530

    done

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -4,20 +4,12 @@ const {
   lookupServiceHistory, 
   createServiceRecord, 
   updateServiceRecord,
-  deleteServiceRecord,
-  getServiceStats,
-  filterServiceRecords
+  deleteServiceRecord
 } = require("../controllers/serviceController");
 const protect = require("../middleware/authMiddleware");
 
 // Look up a product's service history & warranty status
 router.get("/history", lookupServiceHistory);
-
-// Get service stats for tiles
-router.get("/stats", protect, getServiceStats);
-
-// Filter service records
-router.get("/filter", protect, filterServiceRecords);
 
 // Create a new service record (Store Accept)
 router.post("/", protect, createServiceRecord);
